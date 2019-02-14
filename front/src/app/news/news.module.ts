2ba861069eda3120c13ab3568156457629adf4ae
Thomas Leveque <thomasl5895@gmail.com>
2019-02-14 15:15:52 +0100

    modules for news module

--- front/src/app/news/news.module.ts
+++ front/src/app/news/news.module.ts
@@ -3,11 +3,21 @@ import { CommonModule } from '@angular/common';
 import { NewsListComponent } from './news-list/news-list.component';
 import { NewsCreateComponent } from './news-create/news-create.component';
 import { NewsEditComponent } from './news-edit/news-edit.component';
+import { ServiceModule } from '../services/services.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '../material.module';
+import { RouterModule } from '@angular/router';
+import { routes } from '../app.routes';
 
 @NgModule({
   declarations: [NewsListComponent, NewsCreateComponent, NewsEditComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ServiceModule,
+    FormsModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes)
   ]
 })
-export class NewsModule { }
+export class NewsModule {}
